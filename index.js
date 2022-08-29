1940539248f5f749f15a070a38b3aa09feff9011
[Ram Dherange] <[ramdherange30@gmail.com]>
2022-08-29 17:25:16 +0530

    file will be updated

---
+++ index.js
@@ -0,0 +1,19 @@
+let marks=21;
+
+if(marks<10) {
+    console.log("you are fail");
+}
+else if (marks>10 && marks<15)
+ {
+    console.log("average marks");}
+
+else if (marks>15 && marks<20)
+ {
+        console.log("good marks");
+} 
+else {
+    console.log("excelent marks");
+}
+
+
+
